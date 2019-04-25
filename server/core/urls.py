1885ffee39674f03ccf3e2f0c68c243138508390
cjdango <cjdango@gmail.com>
2019-04-25 13:21:38 +0800

    Add login to the browsable API.

--- server/core/urls.py
+++ server/core/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf.urls import include
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('boards/', include('boards.urls')),
+]
+
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
 ]
