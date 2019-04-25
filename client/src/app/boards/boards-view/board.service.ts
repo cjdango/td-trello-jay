dee4c844186d78210d2f88953240b7d0b782362a
cjdango <cjdango@gmail.com>
2019-04-25 14:17:15 +0800

    Add board service with fetchBoardList method.

---
+++ client/src/app/boards/boards-view/board.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BoardService {
+  private boardsURL = 'boards'
+
+  constructor(
+    private http: HttpClient
+  ) {
+  }
+
+  fetchBoardList(): Observable<any> {
+    const url = `${this.boardsURL}/`
+    return this.http.get<any>(url)
+  }
+}
--- client/src/app/boards/boards-view/boards-view.component.ts
+++ client/src/app/boards/boards-view/boards-view.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { BoardService } from './board.service';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-boards-view',
   templateUrl: './boards-view.component.html',
   styleUrls: ['./boards-view.component.scss']
 })
 export class BoardsViewComponent implements OnInit {
+  boards: Array<any>
 
-  constructor() { }
+  constructor(
+    private boardService: BoardService
+  ) { }
 
   ngOnInit() {
+    this.getBoards()
+  }
+
+  private getBoards() {
+    this.boardService.fetchBoardList().subscribe(boards => {
+      this.boards = boards
+    })
   }
 
 }
