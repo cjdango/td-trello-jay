3c6fd118119dda9234111ca47d67cb085ddaf0e2
cjdango <cjdango@gmail.com>
2019-05-01 10:41:33 +0800

    Register card in admin.

--- server/boards/admin.py
+++ server/boards/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Board, List
+from .models import Board, List, Card
 
 
 admin.site.register(Board)
 admin.site.register(List)
+admin.site.register(Card)
