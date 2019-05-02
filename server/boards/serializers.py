075bd92d46e6aaa661b116815666581c1c21bb20
cjdango <cjdango@gmail.com>
2019-05-02 14:43:16 +0800

    Include card_set in ListSerializer.

--- server/boards/serializers.py
+++ server/boards/serializers.py
@@ -17,17 +17,6 @@ class BoardSerializer(serializers.HyperlinkedModelSerializer):
         board.members.add(validated_data['owner'])
         return board
 
-
-class ListSerializer(serializers.HyperlinkedModelSerializer):
-    url = serializers.HyperlinkedIdentityField(view_name="boards:list_fetch")
-    board = serializers.PrimaryKeyRelatedField(read_only=True)
-    is_archived = serializers.BooleanField(write_only=True)
-    
-    class Meta:
-        model = List
-        fields = ['pk', 'url', 'position', 'is_archived', 'board', 'title']
-
-
 class CardSerializer(serializers.HyperlinkedModelSerializer):
     url = serializers.HyperlinkedIdentityField(view_name="boards:card_fetch")
     lst = serializers.PrimaryKeyRelatedField(queryset=List.objects.all())
@@ -36,3 +25,15 @@ class CardSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Card
         fields = ['pk', 'url', 'position', 'is_archived', 'lst', 'title']
+
+
+class ListSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="boards:list_fetch")
+    board = serializers.PrimaryKeyRelatedField(read_only=True)
+    is_archived = serializers.BooleanField(write_only=True)
+    card_set = CardSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = List
+        fields = ['pk', 'url', 'position', 'is_archived', 'board', 'title', 'card_set']
+
