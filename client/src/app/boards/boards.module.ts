0e1427dbf03217a2b1939149102b04674a91a8c8
cjdango <cjdango@gmail.com>
2019-05-02 12:49:52 +0800

    Generate board-details component.

--- client/src/app/boards/boards.module.ts
+++ client/src/app/boards/boards.module.ts
@@ -5,9 +5,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatModule } from '../mat.module';
 import { BoardsViewComponent } from './boards-view/boards-view.component';
 import { AddBoardFormComponent } from './add-board-form/add-board-form.component';
+import { BoardDetailsComponent } from './board-details/board-details.component';
 
 @NgModule({
-  declarations: [BoardsViewComponent, AddBoardFormComponent],
+  declarations: [BoardsViewComponent, AddBoardFormComponent, BoardDetailsComponent],
   imports: [CommonModule, ReactiveFormsModule, MatModule]
 })
 export class BoardsModule {}
